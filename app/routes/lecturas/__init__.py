cc7d1ab52e77c93b8b4b088906456708e81abc16
Francsico Arancibia <frarancibia.g@gmail.com>
2025-12-21 21:04:26 -0300

    "nada"

--- app/routes/lecturas/__init__.py
+++ app/routes/lecturas/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.routes.lecturas.libros import router as libros_router
-from app.routes.lecturas.registro_lectura import router as lecturas_router
+from .libros import router as libros_router
+from .registro_lectura import router as lecturas_router
 
 
 router = APIRouter(prefix="/lecturas")
