aafedba6245830552f56bba199f067d6c1780fe4
Francsico Arancibia <frarancibia.g@gmail.com>
2025-12-31 11:01:32 -0300

    "cambios en la llamada de los datos"

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,14 +1,14 @@
-from app.models.usuario import Usuario, Mensaje
-from app.models.habitos import CategoriaHabito, Habito, RegistroHabito
-from app.models.lecturas import Lectura, RegistroLectura
-from app.models.finanzas import (
+from .usuario import Usuario, Mensaje
+from .habitos import CategoriaHabito, Habito, RegistroHabito
+from .lecturas import Lectura, RegistroLectura
+from .finanzas import (
     EnumCuentas,
     Banco, 
     CategoriaFinanza, 
     CuentaBancaria, 
     Movimiento
 )
-from app.models.entrenamiento import (
+from .entrenamiento import (
     Ejercicios, 
     Entrenamiento, 
     EntrenamientoAerobico, 
